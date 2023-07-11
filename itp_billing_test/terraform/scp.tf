456818d84aaec3a0f4d6b30b1afc8e450a6b7e38
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-07-11 16:25:22 +1000

    Update scp.tf

--- itp_billing_test/terraform/scp.tf
+++ itp_billing_test/terraform/scp.tf
@@ -1,5 +1,5 @@
 ## Create a Security Policy to restrict EC2 type 
-data "aws_iam_policy_document" "restrict_ec2_types" {
+data "aws_iam_policy_document" "restrict_ec2_type" {
   statement {
     sid       = "RestrictEc2Types"
     effect    = "Deny"
@@ -19,14 +19,14 @@ data "aws_iam_policy_document" "restrict_ec2_types" {
   }
 }
 
-resource "aws_organizations_policy" "restrict_ec2_types" {
-  name        = "restrict_ec2_types"
+resource "aws_organizations_policy" "restrict_ec2_type" {
+  name        = "restrict_ec2_type"
   description = "Allow certain EC2 instance types only."
-  content     = data.aws_iam_policy_document.restrict_ec2_types.json
+  content     = data.aws_iam_policy_document.restrict_ec2_type.json
 }
 
  #### To attch policy on Organization Account ########
  resource "aws_organizations_policy_attachment" "test-scp-custom" {
-   policy_id = aws_organizations_policy.restrict_ec2_types.id  ## (Required) The unique identifier (ID) of the policy that you want to attach to the target.
+   policy_id = aws_organizations_policy.restrict_ec2_type.id  ## (Required) The unique identifier (ID) of the policy that you want to attach to the target.
   target_id = "ou-gggb-0k7see5q"  ## (Required) The unique identifier (ID) organizational unit, you want to attach the policy to.
  }
