ff256c211351b1cfdfd256ee6aae448637fe9744
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-07-11 13:33:59 +1000

    Update ddb.tf

--- sandbox/terraform/ddb.tf
+++ sandbox/terraform/ddb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "GameScores"
-  billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
+  billing_mode   = "PROVISIONED"
   hash_key       = "UserId"
   range_key      = "GameTitle"
 
