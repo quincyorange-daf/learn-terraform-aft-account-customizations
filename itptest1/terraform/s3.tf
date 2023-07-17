a8cee6085c8ee4c37c31c8fa2f6f653dd23b9e03
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-07-17 13:41:20 +1000

    Update s3.tf

--- itptest1/terraform/s3.tf
+++ itptest1/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "itptest4_bucket" {
-  bucket = "aft-itptest4_bucket-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "itptest4-bucket" {
+  bucket = "aft-itptest4-bucket-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
